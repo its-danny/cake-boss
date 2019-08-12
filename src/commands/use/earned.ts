3293159a8affe929e0a96ca32008b95e2581e8f5
Danny Tatom <dannytatom@gmail.com>
2019-08-12 01:02:12 -0700

    Add -earned command

---
+++ src/commands/use/earned.ts
@@ -0,0 +1,40 @@
+import { Argv } from 'yargs';
+import { Client, Message } from 'discord.js';
+import Server from '../../entity/server';
+
+interface Arguments {
+  [x: string]: unknown;
+  client: Client;
+  message: Message;
+  needsFetch: boolean;
+  promisedOutput: Promise<string> | null;
+}
+
+export const getEarned = async (args: Arguments): Promise<string> => {
+  const server = await Server.findOne({
+    where: { discordId: args.message.guild.id },
+    relations: ['config', 'members'],
+  });
+
+  if (server) {
+    const member = server.members.find(mem => mem.discordId === args.message.member.id);
+
+    if (!member) {
+      return `You ain't got any!`;
+    }
+
+    return `You've earned a total of ${member.earned} ${server.config.cakeEmoji}!`;
+  }
+
+  throw new Error('Could not find server.');
+};
+
+export const command = 'earned';
+export const describe = `View how many cakes you've earned over time`;
+
+export const builder = (yargs: Argv) => yargs;
+
+export const handler = (args: Arguments) => {
+  args.needsFetch = true;
+  args.promisedOutput = getEarned(args);
+};
