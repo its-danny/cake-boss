fa3a86920875e3be63f420f5e889b99ba67d25b7
Danny Tatom <dannytatom@gmail.com>
2019-08-12 11:22:40 -0700

    Add -disappear and -remove commands

---
+++ src/commands/use/disappear.ts
@@ -0,0 +1,82 @@
+import { Argv } from 'yargs';
+import { Client, Message } from 'discord.js';
+import Server from '../../entity/server';
+import { canManage } from '../../utils/permissions';
+import Member from '../../entity/member';
+import { logEvent } from '../../utils/logger';
+
+interface Arguments {
+  [x: string]: unknown;
+  client: Client;
+  message: Message;
+  member: string;
+  amount?: number;
+  needsFetch: boolean;
+  promisedOutput: Promise<string> | null;
+}
+
+export const disappearCakes = async (args: Arguments): Promise<string> => {
+  if (!(await canManage(args.message))) {
+    return `😝 You ain't got permission to do that!`;
+  }
+
+  const server = await Server.findOne({
+    where: { discordId: args.message.guild.id },
+    relations: ['config', 'members'],
+  });
+
+  if (!server) {
+    throw new Error('Could not find server.');
+  }
+
+  await args.message.guild.fetchMembers();
+
+  const memberId = args.member.replace(/^<@!?/, '').replace(/>$/, '');
+  const discordMember = args.message.guild.members.get(memberId);
+
+  if (!discordMember) {
+    return `😢 Uh oh, I couldn't find them.`;
+  }
+
+  const member = await Member.findOne({ where: { discordId: discordMember.id } });
+
+  if (!member) {
+    return `😢 Uh oh, I couldn't find them.`;
+  }
+
+  const amount = args.amount ? args.amount : 1;
+
+  if (!Number.isInteger(amount) && amount <= 0) {
+    return '😨 Invalid amount, sorry!';
+  }
+
+  if (member.balance > 0) {
+    member.balance -= 1;
+  }
+
+  if (member.earned > 0) {
+    member.earned -= 1;
+  }
+
+  await member.save();
+
+  logEvent(
+    args.client,
+    args.message,
+    `${server.config.cakeEmoji} \`${args.message.author.tag}\` disappeared ${amount} ${
+      amount > 1 ? server.config.cakeNamePlural : server.config.cakeNameSingular
+    } from \`${discordMember.user.tag}\`.`,
+  );
+
+  return '😁 Done!';
+};
+
+export const command = 'disappear <member> [amount]';
+export const describe = `Erase some cakes from history (when they're being mean)`;
+
+export const builder = (yargs: Argv) => yargs;
+
+export const handler = (args: Arguments) => {
+  args.needsFetch = true;
+  args.promisedOutput = disappearCakes(args);
+};
