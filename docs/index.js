f5a40d4f7232df267dab02c5046e2d876aff800e
Danny Tatom <dannytatom@gmail.com>
2019-08-14 10:47:41 -0700

    Fix API links, add online status indicator in docs

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
-/* global Vue, axios */
+/* global $, Vue, axios */
 /* eslint-disable no-new */
 
 new Vue({
-  el: '#leaderboard',
+  el: '#leaderboard-wrap',
 
   data() {
     return {
@@ -13,13 +13,34 @@ new Vue({
 
   mounted() {
     axios
-      .get('http://159.65.181.207:3000/leaderboard')
+      .get('https://rabitrup.com/leaderboard')
       .then(response => {
         this.topUsers = response.data.topUsers;
         this.topServers = response.data.topServers;
 
-        this.$el.querySelector('#leaderboard-content').classList = '';
+        this.$el.querySelector('#leaderboard').classList = '';
       })
       .catch(() => {});
+  },
+});
+
+new Vue({
+  el: '#status-wrap',
+
+  data() {
+    return {
+      isOnline: false,
+    };
+  },
+
+  mounted() {
+    axios
+      .get('https://rabitrup.com/ping')
+      .then(() => {
+        this.isOnline = true;
+      })
+      .catch(() => {}).finally(() => {
+        $('[data-toggle="tooltip"]').tooltip();
+      });
   },
 });
