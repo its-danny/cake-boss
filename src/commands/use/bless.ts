1a85e2269586bdd52e6940eed806ce0e44b99a9d
Danny Tatom <dannytatom@gmail.com>
2019-08-19 22:58:42 -0700

    Add ability to bless roles

--- src/commands/use/bless.ts
+++ src/commands/use/bless.ts
@@ -8,6 +8,7 @@ import { CommandArguments } from '../../utils/command-arguments';
 
 export interface Arguments extends CommandArguments {
   member: string;
+  role: string;
   amount?: number;
 }
 
@@ -67,12 +68,74 @@ export const blessMember = async (args: Arguments): Promise<string | void> => {
 
     return undefined;
   }
+
   return `${server.config.cakeEmoji} They just got ${amount} ${
     amount > 1 ? server.config.cakeNamePlural : server.config.cakeNameSingular
   }, <@${args.message.member.id}>!`;
 };
 
-export const command = 'bless <member> [amount]';
+export const blessRole = async (args: Arguments): Promise<string | void> => {
+  if (!(await canBless(args.message))) {
+    return `${EMOJI_INCORRECT_PERMISSIONS} You ain't got permission to do that!`;
+  }
+
+  const server = await Server.findOne({ where: { discordId: args.message.guild.id } });
+
+  if (!server) {
+    throw new Error('Could not find server.');
+  }
+
+  const discordRole = args.message.guild.roles.find(role => role.name === args.role);
+
+  if (!discordRole) {
+    return `${EMOJI_RECORD_NOT_FOUND} Uh oh, I couldn't find that role.`;
+  }
+
+  const amount = args.amount ? args.amount : 1;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const discordMember of discordRole.members.values()) {
+    // eslint-disable-next-line no-await-in-loop
+    if (!(await isShamed(server.discordId, discordMember.id))) {
+      // eslint-disable-next-line no-await-in-loop
+      const member = await Member.findOrCreate(server.discordId, discordMember.user.id, discordMember.id);
+
+      member.earned += amount;
+      member.balance += amount;
+
+      // eslint-disable-next-line no-await-in-loop
+      await member.save();
+    }
+  }
+
+  logEvent(
+    args.client,
+    args.message,
+    `${server.config.cakeEmoji} \`${args.message.author.tag}\` blessed the \`${
+      discordRole.name
+    }\` role with ${amount} ${amount > 1 ? server.config.cakeNamePlural : server.config.cakeNameSingular}!`,
+  );
+
+  if (server.config.quietMode) {
+    let react: string;
+
+    if (/\b:\d{18}/.test(server.config.cakeEmoji)) {
+      [react] = server.config.cakeEmoji.match(/\d{18}/)!;
+    } else {
+      react = server.config.cakeEmoji;
+    }
+
+    args.message.react(react);
+
+    return undefined;
+  }
+
+  return `${server.config.cakeEmoji} They just got ${amount} ${
+    amount > 1 ? server.config.cakeNamePlural : server.config.cakeNameSingular
+  }, <@${args.message.member.id}>!`;
+};
+
+export const command = 'bless <member|role> [amount]';
 export const describe = 'Bless them with cakes';
 
 export const builder = (yargs: Argv) => yargs;
@@ -80,5 +143,10 @@ export const builder = (yargs: Argv) => yargs;
 export const handler = (args: Arguments) => {
   args.needsFetch = true;
   args.careAboutQuietMode = true;
-  args.promisedOutput = blessMember(args);
+
+  if (/^<@!(\d*)>/.test(args.member)) {
+    args.promisedOutput = blessMember(args);
+  } else {
+    args.promisedOutput = blessRole(args);
+  }
 };
