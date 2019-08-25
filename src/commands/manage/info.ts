5965436f169df884f2e916a92cf55b2113e4142b
Danny Tatom <dannytatom@gmail.com>
2019-08-24 19:02:22 -0700

    Remove link command, add info command

---
+++ src/commands/manage/info.ts
@@ -0,0 +1,21 @@
+import { Argv } from 'yargs';
+import { CommandArguments } from '../../utils/command-arguments';
+
+export const command = 'info';
+export const describe = `What is Cake Boss?`;
+
+export const builder = (yargs: Argv) => yargs;
+
+export const handler = (args: CommandArguments) => {
+  args.promisedOutput = new Promise(resolve =>
+    resolve(`
+    > Someone help you out recently? Just give 'em cake!
+
+    Cake Boss is a discord bot that lets users reward each other cakes for being helpful, and redeem those cakes for prizes.
+
+    GitHub: <https://github.com/dannytatom/cake-boss>
+    Docs: <https://dannytatom.github.io/cake-boss/>
+    Support: <https://discord.gg/2AG9fKt>
+  `),
+  );
+};
