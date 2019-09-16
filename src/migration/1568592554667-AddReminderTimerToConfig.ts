81a42cb23939f79bf1f0a8f4a780851185040b8e
Danny Tatom <dannytatom@gmail.com>
2019-09-15 17:16:42 -0700

    Add config option for how long redeem menu should stick around

---
+++ src/migration/1568592554667-AddReminderTimerToConfig.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddReminderTimerToConfig1568592554667 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "config" ADD "redeemTimer" integer NOT NULL DEFAULT 10`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`ALTER TABLE "config" DROP COLUMN "redeemTimer"`);
+    }
+
+}
