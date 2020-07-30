0ff72bd23ae5896973b3758cc48098d16b569881
Danny Tatom <dannytatom@gmail.com>
2020-07-30 11:42:17 -0700

    fix(ormconfig.js): update entity path

--- ormconfig.js
+++ ormconfig.js
@@ -9,9 +9,9 @@ module.exports = {
   synchronize: false,
   logging: false,
 
-  entities: [process.env.NODE_ENV === "production" ? "dist/entity/**/*.js" : "src/entity/**/!(*.test.ts)"],
-  migrations: [process.env.NODE_ENV === "production" ? "dist/migration/**/*.js" : "src/migration/**/!(*.test.ts)"],
-  subscribers: [process.env.NODE_ENV === "production" ? "dist/subscriber/**/*.js" : "src/subscriber/**/!(*.test.ts)"],
+  entities: [process.env.NODE_ENV === "production" ? "dist/src/entity/**/*.js" : "src/entity/**/!(*.test.ts)"],
+  migrations: [process.env.NODE_ENV === "production" ? "dist/src/migration/**/*.js" : "src/migration/**/!(*.test.ts)"],
+  subscribers: [process.env.NODE_ENV === "production" ? "dist/src/subscriber/**/*.js" : "src/subscriber/**/!(*.test.ts)"],
 
   cli: {
     entitiesDir: "src/entity",
