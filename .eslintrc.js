36e16bf8f4e116218b3a9fd4b1c6ddeb53ef1be0
Danny Tatom <dannytatom@gmail.com>
2020-08-16 15:58:04 -0700

    chore: remove duplicate eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "import/no-cycle": "off",
     "import/prefer-default-export": "off",
-    "simple-import-sort/sort": "error",
     "no-console": "off",
     "no-param-reassign": "off",
     "no-return-assign": "off",
